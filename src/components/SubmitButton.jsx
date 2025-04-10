9380c17bfef1c2319c621400e8e61b6df0b92cd6
natchaya.s <natchaya_sir@outlook.com>
2025-04-10 22:50:11 +0700

    Fix: Disable submit button when submitting form.

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,12 +1,18 @@
+import { useNavigation } from 'react-router-dom';
+
 const SubmitButton = ({ text, color, tracking }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <button
       type='submit'
       className={`btn btn-block uppercase ${
         color ? color : 'btn-accent'
       } ${tracking}`}
+      disabled={isSubmitting}
     >
-      {text}
+      {isSubmitting ? 'submitting...' : text}
     </button>
   );
 };
